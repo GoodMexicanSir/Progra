4e7049fa946425a424b5f9fa4c4c2be5d4e4003a
GoodMexicanSir <gamemoxion21@hotmail.com>
2017-11-11 19:54:22 -0600

    Add files via upload

---
+++ web/Show_and_Hide.js
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function show_and_hide (param1) {
+    hide(document.querySelectorAll('.col'));
+    var elements = param1;
+    show(elements);
+}
+
+function hide (elements) {
+  elements = elements.length ? elements : [elements];
+  for (var index = 0; index < elements.length; index++) {
+    elements[index].style.display = 'none';
+  }
+}
+
+function show (elements, specifiedDisplay) {
+  elements = elements.length ? elements : [elements];
+  for (var index = 0; index < elements.length; index++) {
+    elements[index].style.display = specifiedDisplay || 'block';
+  }
+}
+
